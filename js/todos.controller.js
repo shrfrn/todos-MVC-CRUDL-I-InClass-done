8b1dede12a2adae7c7a0e1a09deb61806cf05fbe
shrfrn <shrfrn@gmail.com>
2024-01-29 12:39:13 +0200

    Seperate controller and service - MVC

--- js/todos.controller.js
+++ js/todos.controller.js
@@ -1,18 +1,13 @@
 'use strict'
 
-var gTodos = [
-    { id: 't101', txt: 'Do this', isDone: false },
-    { id: 't102', txt: 'Do that', isDone: true },
-    { id: 't103', txt: 'Try this', isDone: false },
-]
-
 function onInit() {
     renderTodos()
 }
 
 function renderTodos() {
     const elTodoList = document.querySelector('.todo-list')
-	const strHtmls = gTodos.map(todo => `
+	const todos = getTodos()
+    const strHtmls = todos.map(todo => `
         <li onclick="onToggleTodo('${todo.id}')">
             <span class="${todo.isDone ? "done" : ""}">${todo.txt}</span>
             <button onclick="onRemoveTodo(event, '${todo.id}')">x</button>
@@ -23,29 +18,20 @@ function renderTodos() {
 function onRemoveTodo(ev, todoId) {
     ev.stopPropagation()
     
-    const idx = gTodos.findIndex(todo => todo.id === todoId)
-    gTodos.splice(idx, 1)
-    
+    removeTodo(todoId)
     renderTodos()
 }
 
 function onToggleTodo(todoId){
-    console.log(todoId)
-    const todo = gTodos.find(todo => todo.id === todoId)
-    todo.isDone = !todo.isDone
-    
+    toggleTodo(todoId)    
     renderTodos()
 }
 
 function onAddTodo() {
 	const elInput = document.querySelector('.new-todo')
-    const todo = {
-        id: 't' + Date.now() % 1000,
-        txt: elInput.value,
-        isDone: false,
-    }
-    gTodos.unshift(todo)
-    
+
+    addTodo(elInput.value)
     renderTodos()
+
 	elInput.value = ''
 }
