570fed2ab92a154b93879cbd4dae311ea834a617
shrfrn <shrfrn@gmail.com>
2024-01-29 12:16:42 +0200

    Implement basic dynamic rendering

--- js/todos.js
+++ js/todos.js
@@ -1,11 +1,24 @@
 'use strict'
 
+function onInit() {
+    renderTodos()
+}
+
+function renderTodos() {
+    const elTodoList = document.querySelector('.todo-list')
+	const strHtml = `<li>
+        <span class="done">Do that</span>
+        <button onclick="onRemoveTodo('t456')">x</button>
+    </li>`
+    elTodoList.innerHTML = strHtml
+}
+
 function onRemoveTodo(todoId) {
-    console.log('todoId: ', todoId)
+	console.log('todoId: ', todoId)
 }
 
 function onAddTodo() {
-    const elInput = document.querySelector('.new-todo')
-    console.log(elInput.value)
-    elInput.value = ''
+	const elInput = document.querySelector('.new-todo')
+	console.log(elInput.value)
+	elInput.value = ''
 }
