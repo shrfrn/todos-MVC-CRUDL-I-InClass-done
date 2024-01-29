f5fdd26fec3bc4fdabf76f3dab1889aa97faa1c4
shrfrn <shrfrn@gmail.com>
2024-01-29 12:14:06 +0200

    Add basic functionality

--- js/todos.js
+++ js/todos.js
@@ -1,1 +1,11 @@
-console.log('Hi')
+'use strict'
+
+function onRemoveTodo(todoId) {
+    console.log('todoId: ', todoId)
+}
+
+function onAddTodo() {
+    const elInput = document.querySelector('.new-todo')
+    console.log(elInput.value)
+    elInput.value = ''
+}
