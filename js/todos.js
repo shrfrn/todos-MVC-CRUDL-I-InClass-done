8b37390ff0659995b4081cbdb8946f282531f2c8
shrfrn <shrfrn@gmail.com>
2024-01-29 12:20:47 +0200

    Render from model

--- js/todos.js
+++ js/todos.js
@@ -1,16 +1,23 @@
 'use strict'
 
+var gTodos = [
+    { id: 't101', txt: 'Do this', isDone: false },
+    { id: 't102', txt: 'Do that', isDone: true },
+    { id: 't103', txt: 'Try this', isDone: false },
+]
+
 function onInit() {
     renderTodos()
 }
 
 function renderTodos() {
     const elTodoList = document.querySelector('.todo-list')
-	const strHtml = `<li>
-        <span class="done">Do that</span>
-        <button onclick="onRemoveTodo('t456')">x</button>
-    </li>`
-    elTodoList.innerHTML = strHtml
+	const strHtmls = gTodos.map(todo => `
+        <li>
+            <span class="${todo.isDone ? "done" : ""}">${todo.txt}</span>
+            <button onclick="onRemoveTodo('${todo.id}')">x</button>
+        </li>`)
+    elTodoList.innerHTML = strHtmls.join('')
 }
 
 function onRemoveTodo(todoId) {
